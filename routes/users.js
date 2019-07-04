f56bcb53cdb10a362d3c013cae4bbc0dd5d8895e
fade <fade511@163.com>
2019-07-04 12:03:35 +0800

    jd day03

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,8 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const express = require('express');
+//创建路由器对象
+var router  = express.Router();
+router.get('/insert',function (err,result) {
+   res.send("123");
 });
-
 module.exports = router;
+
